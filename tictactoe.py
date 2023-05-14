d11b1c1d61331426935284d3f3c845db38018b85
cerberus666 <cerberusofdeath@gmail.com>
2023-05-14 16:00:04 +0800

    create basic pygame window

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,26 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 300
+screen_height = 300
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("TicTacToe")
+
+
+run = True
+while run:
+    # Add event handlers
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+pygame.quit()
+
+
 # Part 1
 # Import modules
 # Create grid and setup the grid list
