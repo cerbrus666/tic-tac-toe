057bca0e95b0173062b00932ed3ca4036ea35f11
cerberus666 <cerberusofdeath@gmail.com>
2023-05-14 16:07:08 +0800

    draw grid

--- tictactoe.py
+++ tictactoe.py
@@ -10,8 +10,18 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("TicTacToe")
 
 
+def draw_grid():
+    bg = (255, 255, 200)
+    grid = (50, 50, 50)
+    screen.fill(bg)
+    for x in range(1, 3):
+        pygame.draw.line(screen, grid, (0, x * 100), (screen_width, x * 100))
+        pygame.draw.line(screen, grid, (x * 100, 0), (x * 100, screen_height))
+
+
 run = True
 while run:
+    draw_grid()
     # Add event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
