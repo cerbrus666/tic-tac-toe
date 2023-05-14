9ddff4e1f7981ad7f54117d8ae1af1d437aa7113
cerberus666 <cerberusofdeath@gmail.com>
2023-05-14 15:53:22 +0800

    basic planning

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+# Part 1
+# Import modules
+# Create grid and setup the grid list
+
+# Part 2
+# Create event handler for clicking on a cell
+# Check if cell is empty and place a marker
+# Setup function for drawing markers onto the grid
+
+
+# Part 3
+# Create check winner function including checks for winning combinations
+# Create event handle for post game
+# Reset game
